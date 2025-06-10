6678bd11eb1125f66abd40ac559a92167f94949e
GuDaeWoong <39939767+GuDaeWoong@users.noreply.github.com>
2025-06-10 13:04:46 +0900

    레벨 3-2: TestCode 연습 및 수정

--- src/main/java/org/example/expert/domain/manager/service/ManagerService.java
+++ src/main/java/org/example/expert/domain/manager/service/ManagerService.java
@@ -35,7 +35,8 @@ public class ManagerService {
         Todo todo = todoRepository.findById(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
-        if (!ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
+        // todo.getUser() == null 추가 해주지않으면 nullpointexception이 발생
+        if (todo.getUser() == null || !ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
             throw new InvalidRequestException("담당자를 등록하려고 하는 유저가 일정을 만든 유저가 유효하지 않습니다.");
         }
 
